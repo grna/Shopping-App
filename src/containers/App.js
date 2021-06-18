83897514968bb546ceb9274c4d7ddacf1b49bb5e
grna <kgirius@gmail.com>
2021-06-18 12:53:17 +0300

    initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">Shopping App</a>
+      </header>
+      <main>Product List</main>
+      <footer>All rights reserved</footer>
+    </div>
+  );
+};
+
+export default App;
